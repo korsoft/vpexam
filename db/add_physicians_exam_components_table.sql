ddfaf7d29eed0e834bdfd51fd7d53edbecab9650
Omar Meraz <omar.meraz@korsoftcorp.com>
2017-10-03 12:04:05 -0700

    VPEX-59: Added the primary key to the table creation. * db/add_physicians_exam_components_table.sql: Added the primary key to the table creation and added an index for the second primary key

--- db/add_physicians_exam_components_table.sql
+++ db/add_physicians_exam_components_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `physicians_exam_components`;
 CREATE TABLE IF NOT EXISTS `physicians_exam_components` (
     `physician_id`          INT(10) UNSIGNED NOT NULL DEFAULT 0,
     `exam_component_abbrev` VARCHAR(10)  NOT NULL DEFAULT '',
-    `created_at`            INT(10) UNSIGNED NOT NULL DEFAULT 0
+    `created_at`            INT(10) UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY(`physician_id`, `exam_component_abbrev`),
+    INDEX idx_pex_exam_component_abbrev (`exam_component_abbrev`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-ALTER TABLE physicians_exam_components ADD PRIMARY KEY(`physician_id`, `exam_component_abbrev`);
