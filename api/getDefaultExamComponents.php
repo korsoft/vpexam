75ca32747e94fbec28eaf148e17242606777f0d0
hector.paredes <hector.paredes@korsoftcorp.com>
2017-10-10 15:34:01 -0700

    VPEX-53: Modified to get exam components in same format to app and web access. * api/getDefaultExamComponents.php: Removed code to get exam component list in same format to app an web access.

--- api/getDefaultExamComponents.php
+++ api/getDefaultExamComponents.php
@@ -4,9 +4,7 @@ include_once '../includes/functions.php';
 
 $response = ['success' => false, 'errorMsg' => ''];
 
-
-if (get_is_browser($_SERVER['HTTP_USER_AGENT'])) {
-    try {
+   try {
         if (!isset($_GET['physId'])) {
             throw new Exception('One or more required parameters was not set.', 1);
         }
@@ -24,42 +22,3 @@ if (get_is_browser($_SERVER['HTTP_USER_AGENT'])) {
     header('Content-Type: application/json');
     echo(json_encode($response,JSON_NUMERIC_CHECK));
     exit();
-}
- else {
-    try {
-    if (!isset($_GET['physId'])) {
-        throw new Exception('One or more required parameters was not set.', 1);
-    }
-    $physId   = $_GET['physId'];
-    $ecString = [];
-
-    $prepStmtGetECString = "SELECT exam_components FROM physician_prefs WHERE id = ?";
-    $stmtGetECString = $mysqli->prepare($prepStmtGetECString);
-    if (!$stmtGetECString) {
-        throw new Exception('Error preparing MySQL insert statement.', 2);
-    }
-    $stmtGetECString->bind_param('i', $physId);
-    $stmtGetECString->execute();
-    $stmtGetECString->bind_result($ecString);
-    $stmtGetECString->fetch();
-    $stmtGetECString->close();
-    
-    if(!empty($_GET['patientId']) && is_numeric($_GET['patientId'])) {
-        $exam = getPatientExam($mysqli, $_GET['patientId'], $physId);
-        if($exam['success']) {
-            $response['exams'] = $exam['data'];
-        }
-        
-    }
-    $response['success']        = true;
-    $response['examComponents'] = explode(',', str_replace(['[', ']', '"'], '', $ecString));
-
-    }
-    catch(Exception $e) {
-        $response['errorMsg'] = $e->getMessage();
-    }
-}
-
-header('Content-Type: application/json');
-echo(json_encode($response));
-exit();
