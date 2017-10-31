f694792132217ffdd51db06219e2db7b3adf9553
argelena87 <argelia.andrade@matt.org>
2017-10-31 12:29:28 -0700

    se suben correcciones en stores

--- db/sps/sp_insert_exam_document.sql
+++ db/sps/sp_insert_exam_document.sql
@@ -1,9 +1,9 @@
 DROP PROCEDURE IF EXISTS sp_insert_exam_document; 
-DELIMITER ;; 
+DELIMITER ;;
 CREATE  PROCEDURE sp_insert_exam_document(IN _exam_id BIGINT(20), IN _name_document VARCHAR(255)) 
-BEGIN 
-	DECLARE _lastid VARCHAR(255);
-	SET _lastid = (SELECT CONCAT( (max(id)+1),'.pdf') FROM patients_documents);
+BEGIN
+    DECLARE _lastid VARCHAR(255);
+    SET _lastid = (SELECT CONCAT((IFNULL(max(id), 0)+1),'.pdf') FROM patients_documents);
     INSERT INTO patients_documents(exam_id, name_document, filename, created_at) VALUES (_exam_id, _name_document, _lastid, UNIX_TIMESTAMP());
     SELECT _lastid AS file;
 END ;;
--- db/sps/sp_select_exam_components_author_physician.sql
+++ db/sps/sp_select_exam_components_author_physician.sql
@@ -2,22 +2,36 @@ DROP PROCEDURE IF EXISTS sp_select_exam_components_author_physician;
 DELIMITER ;;
 CREATE  PROCEDURE sp_select_exam_components_author_physician(IN _physician_id INT(10) UNSIGNED)
 BEGIN
-	SET SESSION group_concat_max_len = 1000000000;
-	SET @query_ids =  CONCAT ('SELECT GROUP_CONCAT(order_exam_components) INTO @num_ids FROM physicians WHERE physician_id=',_physician_id);
-	PREPARE stmt FROM @query_ids;
+    SET SESSION group_concat_max_len = 1000000000;
+    SET @query_ids = CONCAT('
+        SELECT GROUP_CONCAT(order_exam_components) INTO @num_ids 
+        FROM physicians 
+        WHERE physician_id=', _physician_id);
+    
+    PREPARE stmt FROM @query_ids;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-    SET @query = CONCAT('SELECT id, title, type, abbrev, description, sort, public,
-    IF(',_physician_id,' = author_physician, 1, 0) AS author_physician, 
-    IF(created_at IS NOT NULL, 1, 0) AS selected,UNIX_TIMESTAMP(updated_at) AS updated_at 
-    FROM exam_components 
-    LEFT JOIN physicians_exam_components ON exam_component_abbrev = abbrev AND physician_id =', _physician_id, 
-    ' WHERE deleted_at IS NULL
-    ORDER BY FIELD(id,',@num_ids,')');
-    PREPARE stmt FROM @query;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-    
+    IF '' != @num_ids THEN
+        SET @query = CONCAT('
+            SELECT id, title, type, abbrev, description, sort, public,
+                   IF(', _physician_id, ' = author_physician, 1, 0) AS author_physician, 
+                   IF(created_at IS NOT NULL, 1, 0) AS selected,UNIX_TIMESTAMP(updated_at) AS updated_at 
+            FROM exam_components 
+            LEFT JOIN physicians_exam_components ON exam_component_abbrev = abbrev AND physician_id =', _physician_id, '
+            WHERE deleted_at IS NULL
+            ORDER BY FIELD(id, ', @num_ids, ')');
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    ELSE 
+        SELECT id, title, type, abbrev, description, sort, public, 
+        IF(_physician_id = author_physician, 1, 0) AS author_physician, 
+        IF(created_at IS NOT NULL, 1, 0) AS selected,UNIX_TIMESTAMP(updated_at) AS updated_at
+        FROM exam_components 
+        LEFT JOIN physicians_exam_components ON exam_component_abbrev = abbrev AND physician_id = _physician_id 
+        WHERE deleted_at IS NULL
+        ORDER BY sort;
+    END IF;
 END ;;
 DELIMITER ;
