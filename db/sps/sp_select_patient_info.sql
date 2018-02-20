c6e386b029250678628233ba2574a5bf53390695
argelena87 <argelia.andrade@matt.org>
2018-02-20 16:30:41 -0700

    VPEX-113 added register_at at stored sp_select_patient_info

--- db/sps/sp_select_patient_info.sql
+++ db/sps/sp_select_patient_info.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS sp_select_patient_info;
 DELIMITER ;;
 CREATE  PROCEDURE sp_select_patient_info(IN _patientid INT(10) UNSIGNED) 
 BEGIN 
-    SELECT pat.patient_id AS patientId, pat.username, pat.first_name AS firstName, pat.last_name AS lastName, pat.gender, pat.dob, war.entered_at AS waitingroom, war.uploaded
+    SELECT pat.patient_id AS patientId, pat.username, pat.first_name AS firstName, pat.last_name AS lastName, pat.gender, pat.dob, war.entered_at AS waitingroom, war.uploaded, pat.register_at
     FROM patients pat
     LEFT JOIN waiting_room war ON war.patient_id = pat.patient_id
     WHERE pat.patient_id = _patientid;
