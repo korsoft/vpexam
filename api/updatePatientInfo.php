b25702afaf653b846b9b63a3f25c39de2502b609
argelena87 <argelia.andrade@matt.org>
2018-02-19 18:14:14 -0700

    VPEX-120 api and sp to updated the patient information

---
+++ api/updatePatientInfo.php
@@ -0,0 +1,165 @@
+<?php
+include_once '../includes/db_connect.php';
+include_once '../includes/functions.php';
+
+$success = false;
+$errorMsg = "";
+$first_name=""; $middle_name = ""; $last_name= ''; $email = ''; $gender = ''; $phone = ''; $dob = ''; $address = ''; $city = '';
+$state = ''; $zip = ''; $insurance_company = ''; $insurance_address = ''; $insurance_phone = '';$insurance_ph_name = ''; $ins_pat_relationship = ''; 
+$insurance_group_num = ''; $insurance_id_cert_num = ''; $insurance_issue_date = '';
+function validateDate($date, $format = 'Y-m-d')
+{
+    $d = DateTime::createFromFormat($format, $date);
+    return $d && $d->format($format) == $date;
+}
+
+
+try {
+	if (!isset($_POST['patient_id'])) {
+        throw new Exception('One or more required parameters was not set.', 1);
+    }else if ($_POST['patient_id'] == ''){
+    	throw new Exception('One or more required parameters was not set.', 2);
+    }
+    $patId = $_POST['patient_id'];
+    if (isset($_POST['first_name'])) {
+    	$first_name = $_POST['first_name'];
+    	if (strlen($first_name) > 50){
+    		throw new Exception('Invalid first name.', 1);
+    	}
+    	error_log('firstname'. $first_name);
+    }
+    if (isset($_POST['middle_name'])) {
+    	$middle_name = $_POST['middle_name'];
+    	if(strlen($middle_name) > 50){
+    		throw new Exception('Invalid middle name.', 1);
+    	}
+    }
+    if (isset($_POST['last_name'])) {
+        $last_name = $_POST['last_name'];
+    	if(strlen($last_name) > 50){
+    		throw new Exception('Invalid last name.', 1);
+    	}
+    }
+    if (isset($_POST['email'])) {
+    	$email = $_POST['email'];
+        if(strlen($email) > 100){
+    		throw new Exception('Invalid email.', 1);
+    	}else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		  throw new Exception('Invalid email.', 2);
+		}
+    }
+    if (isset($_POST['gender'])) {
+        $gender = $_POST['gender'];
+        if($gender != 'male' && $gender != 'female'){
+    		throw new Exception('Invalid gender.', 1);
+    	}
+    }
+    if (isset($_POST['phone'])) {
+    	$phone = $_POST['phone'];
+        if(strlen($phone)!= 10 ){
+    		throw new Exception('Invalid phone.', 1);
+    	}else if (!preg_match('/[0-9]{10}/', $phone) ) {
+    		throw new Exception('Invalid phone.', 2);
+    	}
+    }
+    if (isset($_POST['dob'])) {
+    	$dob = $_POST['dob'];
+    	error_log(':::: DOB'. $dob);
+        if(strlen($dob)> 12 ){
+    		throw new Exception('Invalid date of birth.', 1);
+    	}else if (!validateDate($dob)) {
+    		throw new Exception('Invalid date of birth.', 2);
+    	}
+    }
+    if (isset($_POST['address'])) {
+    	$address = $_POST['address'];
+        if(strlen($address)> 300 ){
+    		throw new Exception('Invalid address.', 1);
+    	}
+    }
+    if (isset($_POST['city'])) {
+    	$city = $_POST['city'];
+        if(strlen($city)> 200 ){
+    		throw new Exception('Invalid city.', 1);
+    	}
+    }
+    if (isset($_POST['state'])) {
+    	$state = $_POST['state'];
+        if(strlen($state)> 2 ){
+    		throw new Exception('Invalid state.', 1);
+    	}
+    }
+    if (isset($_POST['zip'])) {
+    	$zip = $_POST['zip'];
+        if(strlen($zip)> 9 ){
+    		throw new Exception('Invalid zip code.', 1);
+    	}
+    }
+    if (isset($_POST['insurance_company'])) {
+    	$insurance_company = $_POST['insurance_company'];
+        if(strlen($insurance_company)> 150 ){
+    		throw new Exception('Invalid Insurance Company.', 1);
+    	}
+    }
+    if (isset($_POST['insurance_address'])) {
+    	$insurance_address = $_POST['insurance_address'];
+        if(strlen($insurance_address)> 300 ){
+    		throw new Exception('Invalid Insurance address.', 1);
+    	}
+    }
+    if (isset($_POST['insurance_phone'])) {
+    	$insurance_phone = $_POST['insurance_phone'];
+        if(strlen($insurance_phone)> 15 ){
+    		throw new Exception('Invalid Insurance phone.', 1);
+    	}else if (!preg_match('/[0-9]{10}/', $insurance_phone) ) {
+    		throw new Exception('Invalid phone.', 2);
+    	}
+    }
+    if (isset($_POST['insurance_ph_name'])) {
+    	$insurance_ph_name = $_POST['insurance_ph_name'];
+        if(strlen($insurance_ph_name)> 150 ){
+    		throw new Exception('Invalid Insurante PH name.', 1);
+    	}
+    }
+    if (isset($_POST['insurance_patient_relationship'])) {
+    	$ins_pat_relationship = $_POST['insurance_patient_relationship'];
+        if(strlen($ins_pat_relationship)> 30 ){
+    		throw new Exception('Invalid Insurance patient relationship.', 1);
+    	}
+    }
+    if (isset($_POST['insurance_group_num'])) {
+    	$insurance_group_num = $_POST['insurance_group_num'];
+        if(strlen($insurance_group_num)> 30 ){
+    		throw new Exception('Invalid Insurance group number.', 1);
+    	}
+    }
+    if (isset($_POST['insurance_id_cert_num'])) {
+    	$insurance_id_cert_num = $_POST['insurance_id_cert_num'];
+        if(strlen($insurance_id_cert_num)> 30 ){
+    		throw new Exception('Invalid Insurance id cert num.', 1);
+    	}
+    }
+    if (isset($_POST['insurance_issue_date'])) {
+    	$insurance_issue_date = $_POST['insurance_issue_date'];
+        if(strlen($insurance_issue_date)> 10 ){
+    		throw new Exception('Invalid Insurance issue date.', 1);
+    	}else if (!validateDate($insurance_issue_date)) {
+    		throw new Exception('Invalid Insurance issue date.', 2);
+    	}
+    }
+    //error_log("CALL sp_update_patients_info({$patId},'{$first_name}','{$middle_name}','{$last_name}','{$email}','{$gender}','{$phone}','{$dob}','{$address}','{$city}','{$state}','{$zip}','{$insurance_company}','{$insurance_address}','{$insurance_phone}','{$insurance_ph_name}','{$ins_pat_relationship}','{$insurance_group_num}','{$insurance_id_cert_num}','{$insurance_issue_date}');");
+	$mysqli->query("CALL sp_update_patients_info({$patId},'{$first_name}','{$middle_name}','{$last_name}','{$email}','{$gender}','{$phone}','{$dob}','{$address}','{$city}','{$state}','{$zip}','{$insurance_company}','{$insurance_address}','{$insurance_phone}','{$insurance_ph_name}','{$ins_pat_relationship}','{$insurance_group_num}','{$insurance_id_cert_num}','{$insurance_issue_date}');");
+	error_log('::::::: Despues de llamar sp para cambiar settings');
+	$success = true;
+}
+catch(Exception $e) {
+	$success = false;
+	$errorMsg = $e->getMessage();
+	echo(json_encode(array("success" => $success, "errorMsg" => $errorMsg)));
+    //error_log('::::::: Error al llamar sp para cambiar settings');
+    exit();
+}
+
+$errorMsg = "";
+echo(json_encode(array("success" => $success, "errorMsg" => $errorMsg)));
+exit();
---
+++ db/sps/sp_update_patients_info.sql
@@ -0,0 +1,165 @@
+DROP PROCEDURE IF EXISTS sp_update_patients_info;
+DELIMITER ;;
+CREATE  PROCEDURE sp_update_patients_info(
+	IN _patient_id INT(10) UNSIGNED, 
+	IN _first_name VARCHAR(2048),
+	IN _middle_name VARCHAR(2048),
+	IN _last_name VARCHAR(2048),
+	IN _email VARCHAR(2048),
+	IN _gender VARCHAR(2048),
+	IN _phone VARCHAR(2048),
+	IN _dob VARCHAR(2048),
+	IN _address VARCHAR(2048),
+	IN _city VARCHAR(2048),
+	IN _state VARCHAR(2048),
+	IN _zip VARCHAR(2048),
+	IN _insurance_company VARCHAR(2048),
+	IN _insurance_address VARCHAR(2048),
+	IN _insurance_phone VARCHAR(2048),
+	IN _insurance_ph_name VARCHAR(2048),
+	IN _insurance_patient_relationship VARCHAR(2048),
+	IN _insurance_group_num VARCHAR(2048),
+	IN _insurance_id_cert_num VARCHAR(2048),
+	IN _insurance_issue_date VARCHAR(2048)
+	) 
+BEGIN 
+	DECLARE toupdate VARCHAR(2048) DEFAULT '';
+    IF (_first_name IS NOT NULL AND _first_name != '') THEN
+        SET toupdate = CONCAT('first_name = ', _first_name);
+    END IF;
+    IF (_middle_name IS NOT NULL AND _middle_name != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate,', middle_name = ', _middle_name);
+        ELSE 
+        	SET toupdate = CONCAT('middle_name = ', _middle_name);
+        END IF;	
+    END IF;
+    IF (_last_name IS NOT NULL AND _last_name != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', last_name = ', _last_name);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'last_name = ', _last_name);
+        END IF;
+    END IF;
+    IF (_email IS NOT NULL AND _email != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', email = ', _email);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'email = ', _email);
+        END IF;
+    END IF;
+    IF (_gender IS NOT NULL AND _gender != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', gender = ', _gender);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'gender = ', _gender);
+        END IF;
+    END IF;
+    IF (_phone IS NOT NULL AND _phone != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', phone = ', _phone);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'phone = ', _phone);
+        END IF;
+    END IF;
+    IF (_dob IS NOT NULL AND _dob != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', dob = ', _dob);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'dob = ', _dob);
+        END IF;
+    END IF;
+    IF (_address IS NOT NULL AND _address!= '' ) THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', address = ', _address);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'address = ', _address);
+        END IF;
+    END IF;
+    IF (_city IS NOT NULL AND _city != '') THEN
+	    IF toupdate != '' THEN 
+	        SET toupdate = CONCAT(toupdate, ', city = ', _city);
+	    ELSE
+	    	SET toupdate = CONCAT(toupdate, 'city = ', _city);
+	    END IF;
+    END IF;
+    IF (_state IS NOT NULL AND _state != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', state = ', _state);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'state = ', _state);
+        END IF;
+    END IF;
+    IF (_zip IS NOT NULL  AND _zip != '') THEN
+	    IF toupdate != '' THEN 
+	        SET toupdate = CONCAT(toupdate, ', zip = ', _zip);
+	    ELSE
+	    	SET toupdate = CONCAT(toupdate, 'zip = ', _zip);
+	    END IF;
+    END IF;
+    IF (_insurance_company IS NOT NULL AND _insurance_company != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', insurance_company = ', _insurance_company);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'insurance_company = ', _insurance_company);
+        END IF;
+    END IF;
+    IF (_insurance_address IS NOT NULL AND _insurance_address != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', insurance_address = ', _insurance_address);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'insurance_address = ', _insurance_address);
+        END IF;
+    END IF;
+    IF (_insurance_phone IS NOT NULL AND _insurance_phone != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', insurance_phone = ', _insurance_phone);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'insurance_phone = ', _insurance_phone);
+        END IF;
+    END IF;
+    IF (_insurance_ph_name IS NOT NULL AND _insurance_ph_name != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', insurance_ph_name = ', _insurance_ph_name);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'insurance_ph_name = ', _insurance_ph_name);
+        END IF;
+    END IF;
+    IF (_insurance_patient_relationship IS NOT NULL AND _insurance_patient_relationship != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', insurance_patient_relationship = ', _insurance_patient_relationship);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'insurance_patient_relationship = ', _insurance_patient_relationship);
+        END IF;
+    END IF;
+    IF (_insurance_group_num IS NOT NULL AND _insurance_group_num != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', insurance_group_num = ', _insurance_group_num);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'insurance_group_num = ', _insurance_group_num);
+        END IF;
+    END IF;
+    IF (_insurance_id_cert_num IS NOT NULL AND _insurance_id_cert_num != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', insurance_id_cert_num = ', _insurance_id_cert_num);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'insurance_id_cert_num = ', _insurance_id_cert_num);
+        END IF;
+    END IF;
+    IF (_insurance_issue_date IS NOT NULL AND _insurance_issue_date != '') THEN
+    	IF toupdate != '' THEN 
+        	SET toupdate = CONCAT(toupdate, ', insurance_issue_date = ', _insurance_issue_date);
+        ELSE
+        	SET toupdate = CONCAT(toupdate, 'insurance_issue_date = ', _insurance_issue_date);
+        END IF;
+    END IF;
+
+
+    IF '' != toupdate THEN
+          SET @query = CONCAT('UPDATE patients SET ', toupdate,' WHERE patient_id = ',_patient_id);
+          PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+END ;;
+DELIMITER ;
