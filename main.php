bbaf8499fc1a8376e1a293cfbe00a46c9a02bc26
argelena87 <argelia.andrade@matt.org>
2018-04-12 18:15:42 -0700

    VPEX-139 Home page redisign and adaptative for tablet and cellphone

--- main.php
+++ main.php
@@ -10,6 +10,7 @@
 <html>
     <head>
         <meta content="text/html; charset=windows-1252" http-equiv="content-type">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="msapplication-TileColor" content="#2d89ef">
         <meta name="msapplication-TileImage" content="/mstile-144x144.png">
         <meta name="theme-color" content="#ffffff">
@@ -31,6 +32,7 @@
         <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
         <link rel="stylesheet" type="text/css" href="<?php autoVerCss('/style/style.css');?>">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.0.7/css/swiper.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="manifest" href="/manifest.json">
     </head>
     <body>
@@ -43,8 +45,11 @@
                     <div class="topmenu">
                         <!--<span class="topmenuItem split">|</span>-->
                         <div class="topmenuItem hoverable">
+                             <a href="/"><span>Home</span></a>
+                        </div>
+                        <div class="topmenuItem hoverable">
                             <?php if(0 < $userloggedid) { ?>
-                                <a href="<?php echo true == $_SESSION['is_patient']?'patient_main':'physician_main'; ?>.php"><span>Home</span></a>
+                                <a href="<?php echo true == $_SESSION['is_patient']?'patient_main':'physician_main'; ?>.php"><span>My Patients</span></a>
                             <?php } else { ?>
                                 <span id="btnTopLogin">Login</span>
                             <?php } ?>
@@ -86,93 +91,105 @@
             <div id="content_header"></div>
                 <div id="site_content">
                     <div id="centerContent">
-                        <img src="img/banner.png" alt="Logo Banner" >
-                    </div>
+                       <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                              <li data-target="#myCarousel" data-slide-to="1"></li>
+                              <li data-target="#myCarousel" data-slide-to="2"></li>
+                              <li data-target="#myCarousel" data-slide-to="3"></li>
+                              <li data-target="#myCarousel" data-slide-to="4"></li>
+                              <li data-target="#myCarousel" data-slide-to="5"></li>
+                              <li data-target="#myCarousel" data-slide-to="6"></li>
+                            </ol>
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner">
+                              <div class="item active">
+                                <img src="img/slider/VPExam1.jpg" alt="Los Angeles" style="width:100%;">
+                                <div class="carousel-caption">
+                                  <h3>Your solution to comprehensive personalized telemedicine.</h3>
+                                </div>
+                              </div>
+                              <div class="item">
+                                <img src="img/slider/VPExam2.jpg" alt="Chicago" style="width:100%;">
+                                <div class="carousel-caption">
+                                  <h3>Your solution to comprehensive personalized telemedicine.</h3>
+                                </div>
+                              </div>
+                              <div class="item">
+                                <img src="img/slider/VPExam3.jpg" alt="New York" style="width:100%;">
+                                <div class="carousel-caption">
+                                  <h3>Your solution to comprehensive personalized telemedicine.</h3>
+                                </div>
+                              </div>
+                              <div class="item">
+                                <img src="img/slider/VPExam4.jpg" alt="New York" style="width:100%;">
+                                <div class="carousel-caption">
+                                  <h3>Your solution to comprehensive personalized telemedicine.</h3>
+                                </div>
+                              </div>
+                              <div class="item">
+                                <img src="img/slider/VPExam5.jpg" alt="New York" style="width:100%;">
+                                <div class="carousel-caption">
+                                  <h3>Your solution to comprehensive personalized telemedicine.</h3>
+                                </div>
+                              </div>
+                              <div class="item">
+                                <img src="img/slider/VPExam6.jpg" alt="New York" style="width:100%;">
+                                <div class="carousel-caption">
+                                  <h3>Your solution to comprehensive personalized telemedicine.</h3>
+                                </div>
+                              </div>
+                            </div>
+
+                            <!-- Left and right controls -->
+                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                              <span class="glyphicon glyphicon-chevron-left"></span>
+                              <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                              <span class="glyphicon glyphicon-chevron-right"></span>
+                              <span class="sr-only">Next</span>
+                            </a>
+                          </div>
+                        </div>
                     <!-- Login Div goes here -->
                     <div class="aboutDiv">
-                        <p id="introText">The Virtual Physical Exam platform provides comprehensive, personalized Telemedicine for clinicians and patients.</p>
-                        <br />
-                        <div class="signUpContainerDiv">
-                            <div class="signUpInnerDiv">
-                                <div class="signUpDiv2">
-                                    <h3>Are you a physician?</h3>
-                                    <p>Request your free 30-day trial today.</p>
-                                    <br/>
-                                    <div class="button-dark" <?php echo (0 == $userloggedid?'id="btnRequestTrial"':'') ?>>Create New Account</div>
+                        <div id="contentAbout">
+                            <div class="contenedorAbout">
+                                <img src="img/slider/acercaapp.jpg">
+                                <div class="info">
+                                    <h6>We've made comprehensive telemedicine easy <br >to integrate into your practice.</h6>
+                                    <p>Create your personal account, waiting room, and add it to your practice today.​​​​</p>
                                 </div>
+                            </div>
+                            <div class="infoR">
+                                <img src="img/slider/laptop-exam-overview.jpg"><br>
+                                <a class="watchVideo">Watch Our Video </a>
                             </div>
                         </div>
                         <br /><br />
+
                         <div class="blockDiv">
-                            <h1>Virtual Physical Exam Mobile App</h1>
-                            <p>Your solution to comprehensive, personalized Telemedicine.</p>
+                            <h1>Are you a physician?</h1>
+                            <p>Start your trial today!</p>
                             <br />
-                            <div class="swiper-container">
-                                <div class="swiper-wrapper">
-                                    <div class="swiper-slide">
-                                        <img src="img/1.png">
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="img/2.png">
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="img/3.png">
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="img/4.png">
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="img/5.png">
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="img/6.png">
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="img/7.png">
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="img/8.png">
-                                    </div>
-                                    <div class="swiper-slide">
-                                        <img src="img/9.png">
-                                    </div>
-                                </div>
-                                <!-- Add Pagination -->
-                                <div class="swiper-pagination"></div>
-                                <!-- Add Arrows -->
-                                <div class="swiper-button-next swiper-button-white"></div>
-                                <div class="swiper-button-prev swiper-button-white"></div>
-                            </div>
-                            <br /><br />
-                            <div id="divAvailSoon">
-                                <p>Available soon on Google Play!</p>
-                                <img id="googlePlayLink" src="https://play.google.com/intl/en_us/badges/images/generic/en-play-badge.png" alt="Available on Google Play Logo">
-                            </div>
+                            <div class="createNewAccount" <?php echo (0 == $userloggedid?'id="btnRequestTrial"':'') ?>>Create New Account</div>
                         </div>
                         <div class="descrDiv">
-                            <h1>About VPExam</h1>
-                            <p>
-                                VPExam.com is your Telemedicine Portal for both Patients and Physicians:
-                            </p>
-                            <ul>
-                                <li>Patients can actively participate in their care/view and update your profile/respond to the Virtual History</li>
-                                <li>Physicians' home for reviewing patients' Virtual Physical Exams</li>
-                                <li>Focus on your patients instead of documentation/auto-populate notes and effectively review comprehensive exams</li>
-                                <!--<li>Portal for patients allowing them to view and update profile information</li>
-                                <li>Portal for clinicians allowing them to view patients' exams</li>
-                                <li>Features easy controls for moving from one part of the exam to the next</li>
-                                <li>"Clipboard" feature provided so that clinicians can easily document any abnormalities and make notes</li>
-                                <li>"My Normal" feature allows clinicians to easily fill out the "Clipboard" with predetermined text</li>
-                                <li>Waveform view provides graphical representation of auscultation audio</li>
-                                -->
-                            </ul>
-                            <br /><br />
-                            <h1>Security</h1>
-                            <p>
-                                We take security seriously. <span style="font-weight: bold;">We are 100% HIPAA-compliant.</span> Your sensitive medical information is safe with us.
-                                All software and connections associated with VPExam.com are encrypted through SSL. All medical data is stored securely on our server. Read our HIPAA compliance documentation
-                                <a href="">here</a>.
-                            </p>
+                            <div class="frameYoutube">
+                                <iframe class="ytb-embed" src="https://www.youtube.com/embed/PNkMrz056X0?autoplay=1&controls=0&loop=1&showinfo=0&rel=0" width="730" height="390" frameborder="0" allow="autoplay; encrypted-media" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                            
+                            </div>
+                            <div class="infoY">
+                                <h1>VPExam.com is your Telemedicine Portal for Patients and Physicians​​</h1>
+                                <ul>
+                                    <li>+ Create your personal account, waiting room, and add it to your practice today.</li>
+                                    <li>+ Easy accessibility for patients and clinicians.</li>
+                                    <li>+ HIPAA Compliant.</li>
+                                    <li>+ Access your care from everywhere.</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -187,5 +204,6 @@
         <script type="text/javascript" src="js/sha512.js?time=<?php echo time(); ?>"></script>
         <script type="text/javascript" src="<?php autoVerCss('/js/forms.js');?>"></script>
         <script type="text/javascript" src="js/main.js?time=<?php echo time(); ?>"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </body>
 </html>
